ad7eda26fcb38284c6304c6ad29d1f428031c236
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-27 20:27:05 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,8 @@ def reformat_languages(languages)
     new_language_hash = {}
 
 
-  languages.collect do | level_1 , language |
+  {languages.collect do | level_1 , language |
     new_language_hash.language
-  end
-   new_language_hash
+  end}
+ new_language_hash
 end
